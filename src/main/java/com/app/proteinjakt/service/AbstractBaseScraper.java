a6d8a0124896a470d54ab65505be251951b4ac92
Vikl5 <viktor.klindt@tietoevry.com>
2024-06-04 10:34:03 +0200

    Refactored the main scrape method into smaller methods to improve readability and maintainability in the future

--- src/main/java/com/app/proteinjakt/service/AbstractBaseScraper.java
+++ src/main/java/com/app/proteinjakt/service/AbstractBaseScraper.java
@@ -13,7 +13,7 @@ import java.util.List;
 public abstract class AbstractBaseScraper implements IScraper{
     private final ProductPriceRepository productPriceRepository;
 
-    @Autowired
+
     protected abstract String getProductPriceSelectors();
     protected abstract String getProductNameSelectors();
     protected abstract String getProductOnSaleSelectors();
@@ -24,24 +24,37 @@ public abstract class AbstractBaseScraper implements IScraper{
     }
     @Override
     public void scrape(String url, Elements products) {
-        List<ProteinProduct> prices = new ArrayList<>();
+        List<ProteinProduct> productInfo = new ArrayList<>();
         for (Element prod : products) {
+            ProteinProduct proteinProduct = extractDataFromElement(url, prod);
+            productInfo.add(proteinProduct);
+        }
+        saveProductsToDatabase(productInfo);
+    }
+
+    private void saveProductsToDatabase(List<ProteinProduct> productInfo) {
+        productPriceRepository.saveAll(productInfo);
+    }
+
+    private ProteinProduct extractDataFromElement(String url, Element prod) {
             String productNameSelector = prod.getElementsByClass(getProductNameSelectors()).text();
             String productPriceSelector = prod.getElementsByClass(getProductPriceSelectors()).text();
             String productOnSaleSelector = prod.getElementsByClass(getProductOnSaleSelectors()).text();
             String productUrlSelector = url.concat(prod.select(getProductUrlSelectors()).attr("href"));
-
-            ProteinProduct proteinProduct = new ProteinProduct();
-            proteinProduct.setProductName(productNameSelector);
-            proteinProduct.setWebsite(productUrlSelector);
-            if (productPriceSelector.isEmpty()) {
-                proteinProduct.setPrice(productOnSaleSelector);
-            } else {
-                proteinProduct.setPrice(productPriceSelector);
-            }
-            proteinProduct.setScrapedAt(LocalDateTime.now());
-            prices.add(proteinProduct);
-        }
-        productPriceRepository.saveAll(prices);
+            return setProteinProduct(productNameSelector, productPriceSelector, productUrlSelector, productOnSaleSelector);
     }
+
+    private ProteinProduct setProteinProduct(String name, String price, String website, String sale) {
+        ProteinProduct proteinProduct = new ProteinProduct();
+        proteinProduct.setProductName(name);
+        proteinProduct.setWebsite(website);
+        if (price.isEmpty()) {
+            proteinProduct.setPrice(sale);
+        } else {
+            proteinProduct.setPrice(price);
+        }
+        proteinProduct.setScrapedAt(LocalDateTime.now());
+        return proteinProduct;
+    }
+
 }
